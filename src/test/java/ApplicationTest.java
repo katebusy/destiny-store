b11dd0c2cfa5463c08f53ed156b2dbf5b18b0e8b
Бусыгина Екатерина <0105kate54@gmail.com>
2023-04-15 16:51:36 +0300

    added test context loads

---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,12 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@Slf4j
+@ExtendWith(SpringExtension.class)
+public class ApplicationTest {
+
+  @Test
+  public void contextLoads() {}
+}
